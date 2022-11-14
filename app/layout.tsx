490898c4c37c0c36a58954e6e7b82c7b834167a0
johannranudd <33978097+johannranudd@users.noreply.github.com>
2022-11-14 18:31:56 +0100

    adds --tubo, experimental: { appDir: true }, and app dir

---
+++ app/layout.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import Link from "next/link";
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html>
+      <head></head>
+      <body>
+        <main>
+          <nav className="navbar">
+            <Link href="/">Home</Link>
+            <Link href="/notes">Notes</Link>
+          </nav>
+          {children}
+        </main>
+      </body>
+    </html>
+  );
+}
